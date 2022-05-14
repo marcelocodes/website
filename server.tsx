5057cff770479c306556c27c14a10dcbdf374ac3
Marcelo Almeida <67549662+marcelocodes@users.noreply.github.com>
2022-05-14 20:01:30 +0000

    Fix deno server to support svelte

--- server.tsx
+++ server.tsx
@@ -2,39 +2,80 @@ import { serve } from 'https://deno.land/std@0.114.0/http/server.ts';
 
 const FILE_EXTENSION_TO_CONTENT_TYPE = new Map(
 	Object.entries({
-		'.css': 'text/css',
-		'.js': 'text/javascript',
-		'.woff': 'font/woff',
-		'.woff2': 'font/woff2',
-		'.svg': 'image/svg+xml',
-		'.json': 'application/json',
-		'.html': 'text/html; charset=utf-8',
-		'.png': 'image/png',
-		'.txt': 'text/plain',
-		'.webp': 'image/webp'
+		css: 'text/css',
+		js: 'text/javascript',
+		woff: 'font/woff',
+		woff2: 'font/woff2',
+		svg: 'image/svg+xml',
+		json: 'application/json',
+		html: 'text/html; charset=utf-8',
+		png: 'image/png',
+		txt: 'text/plain',
+		webp: 'image/webp'
 	})
 );
 
-async function handleRequest(request: Request): Promise<Response> {
-	const { pathname } = new URL(request.url);
-	console.log(`Request for: ${pathname}`);
+/**
+ * `pathname` has the format `/favicon.ico`, so we need to change it, to be able
+ * to load from disk.
+ */
+async function loadFile(pathname: string): Promise<Uint8Array> {
+	return await Deno.readFile(`./build${pathname}`);
+}
 
-	const file = await Deno.readFile(pathname);
+// Example:
+//
+//  'filename.css'.split('.') =>
+//  ['filename', 'css'].slice(-1) =>
+//  ['css'][0] =>
+//  'css'
+function getExtension(pathname: string): string {
+	return pathname.split('.').slice(-1)[0];
+}
 
-	if (pathname.length < 4) {
-		console.log(`Invalid pathname: ${pathname}`);
-		Deno.exit(1);
-	}
-
-	const fileExtension = pathname.slice(-4); // 'filename.css' => '.css'
-	const contentType = FILE_EXTENSION_TO_CONTENT_TYPE.get(fileExtension);
+function getContentType(pathname: string): string | Error {
+	const extension = getExtension(pathname);
+	const contentType = FILE_EXTENSION_TO_CONTENT_TYPE.get(extension);
 
 	if (!contentType) {
-		console.log(`Unsupported file type: ${fileExtension}`);
+		return Error(`Invalid extension: ${extension}`);
+	}
+
+	return contentType;
+}
+
+function shouldServeRoot(pathname: string): boolean {
+	if (pathname === '/') {
+		return true;
+	}
+
+	const extension = getExtension(pathname);
+	if (!FILE_EXTENSION_TO_CONTENT_TYPE.get(extension)) {
+		return true;
+	}
+
+	return false;
+}
+
+async function handleRequest(request: Request): Promise<Response> {
+	const { pathname } = new URL(request.url);
+
+	if (shouldServeRoot(pathname)) {
+		return new Response(await loadFile('/index.html'), {
+			headers: {
+				'content-type': 'text/html; charset=utf-8'
+			}
+		});
+	}
+
+	const contentType = getContentType(pathname);
+
+	if (contentType instanceof Error) {
+		console.log(contentType.message);
 		Deno.exit(1);
 	}
 
-	console.log('Serving!');
+	const file = await loadFile(pathname);
 	return new Response(file, {
 		headers: {
 			'content-type': contentType
@@ -42,5 +83,5 @@ async function handleRequest(request: Request): Promise<Response> {
 	});
 }
 
-console.log('Listening on http://localhost:8000');
+console.log('Serving...');
 serve(handleRequest);
