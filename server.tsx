76ac6f46c1fc953f60af4a691e677f028dbfe9fd
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-05-15 13:31:00 +0000

    Update server to use Deno's oak

--- server.tsx
+++ server.tsx
@@ -1,24 +1,64 @@
-import { serve } from './server-deps.ts';
-import { loadRoute, loadResource, load404 } from './server-routes.ts';
+import { bold, cyan, green, red, yellow } from 'https://deno.land/std@0.131.0/fmt/colors.ts';
+import { Application, HttpError, Status } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
+import { join } from 'https://deno.land/std@0.139.0/path/mod.ts';
 
-async function handleRequest(request: Request): Promise<Response> {
-	const { pathname } = new URL(request.url);
+const STATIC_FILES_PATH = join(Deno.cwd(), 'build');
 
-	// Used for html pages.
-	const route = await loadRoute(pathname);
-	if (route) {
-		return route;
+const app = new Application();
+
+// Error handler middleware
+app.use(async (context, next) => {
+	try {
+		await next();
+	} catch (e) {
+		if (e instanceof HttpError) {
+			if (e.status === Status.NotFound) {
+				context.response.status = e.status;
+				context.response.body = await Deno.readFile(join(STATIC_FILES_PATH, '404.html'));
+				return;
+			}
+		} else if (e instanceof Error) {
+			context.response.status = 500;
+			context.response.body = `<!DOCTYPE html>
+            <html>
+              <body>
+                <h1>500 - Internal Server Error</h1>
+              </body>
+            </html>`;
+			console.log('Unhandled Error:', red(bold(e.message)));
+			console.log(e.stack);
+		}
 	}
+});
 
-	// Used for javascript, css, svg, fonts, etc.
-	const resource = await loadResource(pathname);
-	if (resource) {
-		return resource;
-	}
+// Logger
+app.use(async (context, next) => {
+	await next();
+	const rt = context.response.headers.get('X-Response-Time');
+	console.log(
+		`${green(context.request.method)} ${cyan(context.request.url.pathname)} - ${bold(String(rt))}`
+	);
+});
 
-	// Used when a page is not found.
-	return await load404();
-}
+// Response Time
+app.use(async (context, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	context.response.headers.set('X-Response-Time', `${ms}ms`);
+});
 
-console.log('Serving...');
-serve(handleRequest);
+// Send static content
+app.use(async (context) => {
+	await context.send({
+		root: STATIC_FILES_PATH,
+		index: 'index.html'
+	});
+});
+
+app.addEventListener('listen', ({ hostname, port, serverType }) => {
+	console.log(bold('Start listening on ') + yellow(`${hostname}:${port}`));
+	console.log(bold('  using HTTP server: ' + yellow(serverType)));
+});
+
+await app.listen({ hostname: 'localhost', port: 8000 });
